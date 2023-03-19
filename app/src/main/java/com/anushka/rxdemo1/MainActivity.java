21b113b402da0806fefc71b7ac4deeb323f61544
Suleman <sulemankhan447@gmail.com>
2023-03-19 14:19:24 +0530

    Initial commit

---
+++ app/src/main/java/com/anushka/rxdemo1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.anushka.rxdemo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String TAG="myApp";
+    private String greeting="Hello From RxJava";
+
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.tvGreeting);
+
+    }
+}
