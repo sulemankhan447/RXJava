9878ddb2b56d0da88d0419d1446b5ba0db411a56
Suleman <sulemankhan447@gmail.com>
2023-03-19 15:54:05 +0530

    Added comments for disposable

--- app/src/main/java/com/anushka/rxdemo1/MainActivity.java
+++ app/src/main/java/com/anushka/rxdemo1/MainActivity.java
@@ -120,6 +120,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        /**
+         * using disposable to terminate the subscription
+         */
         disposable.dispose();
     }
 }
