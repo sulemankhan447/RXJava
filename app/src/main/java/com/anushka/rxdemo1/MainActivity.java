9086502c1ee0cefb2a3ca506e7b03d6224a84cdb
Suleman <sulemankhan447@gmail.com>
2023-03-19 22:46:10 +0530

    Simplified writing code for rx java

--- app/src/main/java/com/anushka/rxdemo1/MainActivity.java
+++ app/src/main/java/com/anushka/rxdemo1/MainActivity.java
@@ -11,6 +11,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.observers.DisposableObserver;
@@ -18,15 +19,14 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 
 public class MainActivity extends AppCompatActivity {
-    private final static String TAG="myApp";
-    private String greeting="Hello From RxJava";
+    private final static String TAG = "myApp";
+    private String greeting = "Hello From RxJava";
 
     private Observable<String> myObservable;
 
     /**
      * Disposable observer is abstract class which implements observer and disposable
      * disposable observer is more efficient than observable if we have more than one observer in our activity/fragment
-     *
      */
     private DisposableObserver<String> myObserver;
     private DisposableObserver<String> myObserver2;
@@ -36,13 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
 
-//    private Disposable disposable;
+    //    private Disposable disposable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView=findViewById(R.id.tvGreeting);
+        textView = findViewById(R.id.tvGreeting);
         myObservable = Observable.just(greeting);
         /***
          * Observable emits data if there is atleast
@@ -61,71 +61,21 @@ public class MainActivity extends AppCompatActivity {
          * when we request schedule to work with some thread, it picks that thread from thread pool
          * and does work for us
          */
-        myObservable.subscribeOn(Schedulers.io());
+//        myObservable.subscribeOn(Schedulers.io());
 
         /**
          * once all work is done of operators, our observer will receive data emission
          * so that emission we need on ui thread which is main thread
          * so we are telling rx java to observe on main thread
          */
-        myObservable.observeOn(AndroidSchedulers.mainThread());
-//        myObserver =  new Observer<String>() {
-//            @Override
-//            public void onSubscribe(@NonNull Disposable d) {
-//                /**
-//                 * Call when observer successfully subscribes to observable
-//                 */
-//                Log.i("RxJava","onSubscribe Invoked on "+Thread.currentThread().getName());
-//
-//                /**
-//                 * If user has started screen where we do api call and user
-//                 * presses back btn, now activity is detsoryed, but api call is still in progress
-//                 * and once api call returns response
-//                 * it will try to update view
-//                 * as observable thinks, observer is still observing data changed
-//                 * but thats not the case, observable does not know that observer is destroyed
-//                 * so we have to stop observable from publising data changes to avoid
-//                 * memory leak or crash
-//                 * this can be done using disposable object
-//                 * by using disposable object , we will dispose things
-//                 */
-//                disposable = d;
-//            }
-//
-//            @Override
-//            public void onNext(@NonNull String s) {
-//                /**
-//                 * Called when data emission is received
-//                 */
-//                Log.i("RxJava","onNext Invoked on "+Thread.currentThread().getName());
-//                textView.setText(s);
-//            }
-//
-//            @Override
-//            public void onError(@NonNull Throwable e) {
-//                /**
-//                 * Call when we receive error during emission of data
-//                 */
-//                Log.i("RxJava","onError Invoked on "+Thread.currentThread().getName());
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                /**
-//                 * Called when emission of data is completed
-//                 */
-//                Log.i("RxJava","onComplete Invoked on "+Thread.currentThread().getName());
-//            }
-//        };
 
-        myObserver = new DisposableObserver<String>(){
+        myObserver = new DisposableObserver<String>() {
             /**
              * in disposable observer, we dont have
              * onsubscribe method which was used to get disposable object
              * here instead we can direcly use disposable object in on destory
              * refer onDestroy for more details
              */
-
 
 
             @Override
@@ -143,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-        myObserver2 = new DisposableObserver<String>(){
+        myObserver2 = new DisposableObserver<String>() {
             /**
              * in disposable observer, we dont have
              * onsubscribe method which was used to get disposable object
@@ -152,11 +102,10 @@ public class MainActivity extends AppCompatActivity {
              */
 
 
-
             @Override
             public void onNext(@NonNull String s) {
 
-                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
 
             }
 
@@ -177,10 +126,22 @@ public class MainActivity extends AppCompatActivity {
          *
          */
 
-        compositeDisposable.add(myObserver);
-        myObservable.subscribe(myObserver);
-        compositeDisposable.add(myObserver2);
-        myObservable.subscribe(myObserver2);
+//        compositeDisposable.add(myObserver);
+//        myObservable.subscribe(myObserver);
+        /**
+         * so by chaining calls,
+         * we have done subscribe on, observe on and added observer to subscribe with observable
+         * and subscribve with returns observer
+         * since my observer id disposable observer, it returns disposable observer
+         */
+        compositeDisposable.add(myObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(myObserver));
+
+//        compositeDisposable.add(myObserver2);
+//        myObservable.subscribe(myObserver2);
+
+        compositeDisposable.add(myObservable.subscribeWith(myObserver));
 
     }
 
